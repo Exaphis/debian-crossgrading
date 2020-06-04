44bd411e8db5af9e04a2a3c4f14594604e945df0
Exaphis <kevin@kevinniuwu.com>
2020-06-04 13:28:55 -0700

    Verify all initramfs hooks are crossgraded

--- initramfs-package-crossgrade.py
+++ initramfs-package-crossgrade.py
@@ -1,22 +1,36 @@
+from glob import glob
 import subprocess
 import sys
 
-current_arch = subprocess.check_output(['dpkg', '--print-architecture']).strip()
-target_arch = b'arm64'
+current_arch = subprocess.check_output(['dpkg', '--print-architecture'], encoding='UTF-8').strip()
+target_arch = 'arm64'
 
-packages = subprocess.check_output(['dpkg-query', '-f', '${Package}\t${Architecture}\t${Status}\n', '-W']).splitlines()
+packages = subprocess.check_output(['dpkg-query', '-f', '${Package}\t${Architecture}\t${Status}\n', '-W'], encoding='UTF-8').splitlines()
 
+unaccounted_hooks = set(glob('/usr/share/initramfs-tools/hooks/*'))
 crossgrade_targets = []
 for package in packages:
-    name, arch, status = package.split(b'\t')
-    if status == b'install ok installed' and arch != target_arch and arch != b'all':
+    name, arch, status = package.split('\t')
+    if status == 'install ok installed' and arch != target_arch:
         # must specify arch for packages installed in multiple architectures
-        full_name = name + b':' + arch
-        files = subprocess.check_output(['dpkg-query', '-L', full_name]).splitlines()
-        if any(f.startswith(b'/usr/share/initramfs-tools/hooks') for f in files):
-            crossgrade_targets.append(name + b':' + target_arch)
+        full_name = f'{name}:{arch}'
+        files = subprocess.check_output(['dpkg-query', '-L', full_name], encoding='UTF-8').splitlines()
 
-print(f'{len(crossgrade_targets)} targets found.')
-print(crossgrade_targets)
-subprocess.check_call(['apt-get', 'install', crossgrade_targets, '-y'], stdout=sys.stdout, stderr=sys.stderr)
+        num_hooks = len(unaccounted_hooks)
+        unaccounted_hooks.difference_update(files)
+
+        if arch != 'all' and len(unaccounted_hooks) < num_hooks:
+            crossgrade_targets.append(f'{name}:{target_arch}')
+
+if len(unaccounted_hooks) > 0:
+    print('The following hooks in /usr/share/initramfs-tools/hooks are unaccounted for:')
+    for hook in unaccounted_hooks:
+        print(f'\t{hook}')
+    print('Aborting crossgrade.')
+else:
+    print(f'{len(crossgrade_targets)} targets found.')
+    print(crossgrade_targets)
+
+    # crossgrade in one command to prevent repeat triggers (e.g. initramfs rebuild) to save time
+    subprocess.check_call(['apt-get', 'install', *crossgrade_targets, '-y'], stdout=sys.stdout, stderr=sys.stderr)
 
