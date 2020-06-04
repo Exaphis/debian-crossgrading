5034026800ea1082dc5d2db3c176398ea9bf5e1a
Exaphis <kevin@kevinniuwu.com>
2020-06-04 13:43:45 -0700

    Add ldd-edit.sh

---
+++ ldd-edit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+# add dynamic linker for amd64 to ldd
+# to detect shared libraries
+ldd_bak_dir=$(mktemp -d)
+cp /bin/ldd "${ldd_bak_dir}/ldd.bak"
+sed '/^RTLDLIST=.*/a RTLDLIST="${RTLDLIST} /lib64/ld-linux-x86-64.so.2"' /bin/ldd
