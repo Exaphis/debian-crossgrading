8f4aba78c50d00405deb761f27d4f2bd8ad59474
Exaphis <kevin@kevinniuwu.com>
2020-06-12 18:54:18 -0700

    Add explanatory comment to ldd-edit.sh

--- ldd-edit.sh
+++ ldd-edit.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-# add dynamic linker for amd64 to ldd
-# to detect shared libraries
+# add dynamic linker for amd64 to ldd so mkinitramfs can
+# detect and copy shared libraries for amd64 binaries
+
+# might be unnecessary now that all Priority: required/important
+# packages are being crossgraded
+
 ldd_bak_dir=$(mktemp -d)
 cp /bin/ldd "${ldd_bak_dir}/ldd.bak"
 sed -i '/^RTLDLIST=.*/a RTLDLIST="${RTLDLIST} /lib64/ld-linux-x86-64.so.2"' /bin/ldd
