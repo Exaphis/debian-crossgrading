888b32fbc8824f1b82201292b7aaefe303bd574a
Exaphis <kevin@kevinniuwu.com>
2020-06-29 16:13:48 -0700

    Create utils folder, add random_package_install.py
    Create tool to install random packages from dpkg. This will be used
    to test the crossgrade script.
    
    Move all utility scripts into the utils folder.

---
+++ crossgrade/utils/random_install/random_install.py
@@ -0,0 +1,51 @@
+"""Tool to install a given number of random packages from dpkg."""
+import argparse
+import random
+
+import apt
+import apt.progress
+
+def install_random(num_packages, choice_func=None):
+    """Install a given number of random packages, not including dependencies.
+
+    If choice_func is specified, only packages that make choice_func return True
+    are considered.
+    """
+    cache = apt.cache.Cache()
+
+    candidates = set()
+    for package in cache.keys():
+        package_obj = cache[package]
+        if package_obj.is_installed:
+            continue
+        if choice_func is not None and not choice_func(package_obj):
+            continue
+
+        candidates.add(package)
+
+    while num_packages > 0:
+        package = random.choice(candidates)
+        candidates.remove(package)
+
+        cache[package].mark_install()
+        if cache[package].marked_install:
+            print(f'{package} marked for install')
+            num_packages -= 1
+
+    cache.commit(apt.progress.TextFetchProgress(),
+                 apt.progress.InstallProgress())
+
+
+def main():
+    """Main function"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num_packages',
+                        help='The number of random packages that should be installed',
+                        type=int)
+    args = parser.parse_args()
+
+    install_random(args.num_packages)
+
+
+if __name__ == '__main__':
+    main()
