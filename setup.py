cc5d480e48761d7bf3c86755681fe04b9c2f6a71
Exaphis <kevin@kevinniuwu.com>
2020-07-23 19:56:12 -0700

    Prep repo for packaging
    Add setup.py, the build script for setuptools.
    
    Move the utils folder to a separate package since there is
    no direct connection between it and the crossgrader.
    
    Add __init__.py to all package directories to mark them as package
    directories.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+setuptools.setup(
+    name='debian_crossgrader',
+    version='0.0.1',
+    author='Kevin Wu',
+    author_email='kevin@kevinniuwu.com',
+    description='Debian crossgrading tool',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://salsa.debian.org/crossgrading-team/debian-crossgrading',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX :: Linux'
+    ],
+    python_requires='>=3.4',
+    entry_points={
+        'console_scripts': [
+            'crossgrader=debian_crossgrader.crossgrade:main',
+            'package-checker=debian_crosssgrader.crossgrade_utils.package_check:main'
+        ]
+    }
+)
